dd04a1147c10f562e0a08304433f0b4adcb37fae
jagdish <jagdish.bhumca11@gmail.com>
2024-12-03 13:26:29 +0530

    change routes

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ScraperController;
 use App\Http\Controllers\API\ScraperTableController;
 
-Route::get('/message', function () {
-    return response()->json(['message' => 'Hello, this is your message!']);
-});
 Route::get('/scrape', [ScraperController::class, 'scrape']);
 Route::get('/scrapetable', [ScraperTableController::class, 'scrape']);
 
